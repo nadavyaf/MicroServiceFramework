4fce032c6cb2e28dc4fe1d512c78bfdce8f5a8ee
Assiph <asif857@gmail.com>
2021-12-02 15:38:38 +0200

    added pom

---
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -0,0 +1,18 @@
+package bgu.spl.mics;
+
+import junit.framework.TestCase;
+
+public class FutureTest extends TestCase {
+
+    public void testGet() {
+    }
+
+    public void testResolve() {
+    }
+
+    public void testIsDone() {
+    }
+
+    public void testTestGet() {
+    }
+}
