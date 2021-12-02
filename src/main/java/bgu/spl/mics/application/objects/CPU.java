304d833e5c13148f1066e534d31b8ace0a99c279
Assiph <asif857@gmail.com>
2021-12-02 19:33:40 +0200

    finished future tests.

--- src/main/java/bgu/spl/mics/application/objects/CPU.java
+++ src/main/java/bgu/spl/mics/application/objects/CPU.java
@@ -1,9 +1,32 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.LinkedList;
+
 /**
  * Passive object representing a single CPU.
  * Add all the fields described in the assignment as private fields.
  * Add fields and methods to this class as you see fit (including public methods and constructors).
  */
 public class CPU {
+    final private int cores;
+    final private LinkedList<DataBatch> data;
+    final private Cluster cluster;
+
+    public CPU(int numberOfCores, LinkedList<DataBatch> data, Cluster cluster) {
+        this.cores = numberOfCores;
+        this.data = data;
+        this.cluster = cluster;
+    }
+    public int getCores() {
+        return cores;
+    }
+
+    public LinkedList<DataBatch> getData() {
+        return data;
+    }
+
+    public Cluster getCluster() {
+        return cluster;
+    }
+
 }
--- src/test/java/bgu/spl/mics/FutureTest.java
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FutureTest {
@@ -15,13 +17,16 @@ class FutureTest {
 
     @org.junit.jupiter.api.Test
     void TestGet() {
-        assertNotNull(f.get());
+        f.resolve("somevalue");
+        assertNotNull(f.get(),"Got null from get.");
+        assertEquals("somevalue", f.get(), "Expected: result, got: " +f.get());
     }
 
     @org.junit.jupiter.api.Test
     void TestResolve() {
         assertThrows(Exception.class,() -> f.resolve(null), "Expected an error");
         f.resolve("result");
+        assertTrue(f.isDone(),"Already resolved but didn't show");
         assertEquals("result", f.get(), "Expected: result, got: " +f.get());
         assertThrows(Exception.class,() -> f.resolve("something"), "Expected an error");
     }
@@ -35,5 +40,13 @@ class FutureTest {
 
     @org.junit.jupiter.api.Test
     void TestTimeGet() {
+        assertThrows(Exception.class,()-> f.get(-10,TimeUnit.MILLISECONDS),"Worked for a negative time.");
+        long currentTime = System.currentTimeMillis();
+        assertNull(f.get(500,TimeUnit.MILLISECONDS),"Expected null, got a result instead.");
+        assertTrue(System.currentTimeMillis()-currentTime<=500);
+        f.resolve("somevalue");
+        assertNotNull(f.get(10,TimeUnit.MILLISECONDS),"Got null from get.");
+        String ans=f.get(10,TimeUnit.MILLISECONDS);
+        assertEquals("somevalue", ans, "Expected: result, got: " +ans);
     }
 }
