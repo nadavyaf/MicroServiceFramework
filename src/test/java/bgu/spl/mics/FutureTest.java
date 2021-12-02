9a3c994c25bf4a05d50a9452ac536059d6baeffb
nadavyaf <nadavy13@gmail.com>
2021-12-02 15:38:32 +0200

    Added test to future

---
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -0,0 +1,30 @@
+package bgu.spl.mics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FutureTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void resolve() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isDone() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGet() {
+    }
+}
