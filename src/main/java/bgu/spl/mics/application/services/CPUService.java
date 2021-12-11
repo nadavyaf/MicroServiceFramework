984f5415d60b7cec191b81eb8caee548b21b21aa
Assiph <asif857@gmail.com>
2021-12-11 20:40:19 +0200

    finished cpu Service

--- src/main/java/bgu/spl/mics/application/services/CPUService.java
+++ src/main/java/bgu/spl/mics/application/services/CPUService.java
@@ -26,7 +26,7 @@ public class CPUService extends MicroService {
     protected void initialize() {
         MessageBusImpl.getInstance().register(this);
         this.subscribeBroadcast(TickBroadcast.class,callback);
-        Thread t1 = new Thread(()->{
+        Thread t1 = new Thread(()->{//Still waiting for an answer about how to terminate.
             try {
                 this.cpu.process();
             } catch (InterruptedException e) {
